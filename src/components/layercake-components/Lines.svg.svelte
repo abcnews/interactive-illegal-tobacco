220db97ca5080041f3c9f58b7bf3ab67718ceeeb
Joshua Byrd <phocks@gmail.com>
2026-08-03 17:12:08 +1000

    Band aid fix

--- src/components/layercake-components/Lines.svg.svelte
+++ src/components/layercake-components/Lines.svg.svelte
@@ -12,36 +12,46 @@
 
   let { curve }: Props = $props();
 
-  const renderedLines = $derived(
-    $data.flatMap(({ values, config }) => {
-      // Only lines
-      if (config.type !== 'line') return [];
+  const renderedLines = $derived.by(() => {
+    const result = $data
+      .flatMap(({ values, config }) => {
+        // Only lines
+        if (config.type !== 'line') return [];
 
-      // Remove data where y-axis value is null or undefined.
-      const vals = values.flatMap(d => {
-        if (d.y === undefined || d.y === null) return [];
-        return [d];
-      });
+        // Remove data where y-axis value is null or undefined.
+        const vals = values.flatMap(d => {
+          if (d.y === undefined || d.y === null) return [];
+          return [d];
+        });
 
-      // No empty series.
-      if (vals.length === 0) return [];
+        // No empty series.
+        if (vals.length === 0) return [];
 
-      return [
-        {
-          id: config.id,
-          d: line<LayerCakeGroupedDataGroupValuesType>($xGet, $yGet).curve(
-            curve || (config.curveType && curveMap[config.curveType]) || curveCardinal
-          )(vals),
-          stroke: config.colour || (vals[0] ? $zGet(vals[0]) : '#000'),
-          dasharray: config.dasharray
-        }
-      ];
-    })
-  );
+        return [
+          {
+            id: config.id,
+            d: line<LayerCakeGroupedDataGroupValuesType>($xGet, $yGet).curve(
+              curve || (config.curveType && curveMap[config.curveType]) || curveCardinal
+            )(vals),
+            stroke: config.colour || (vals[0] ? $zGet(vals[0]) : '#000'),
+            dasharray: config.dasharray
+          }
+        ];
+      })
+      .sort((a, b) => a.id.localeCompare(b.id));
+
+    // Bandaid fix for split-second duplicate ids
+    // TODO: Fix this properly in the data diffing (data-accessors.ts probably)
+    const deduped = Array.from(new Map(result.map(line => [line.id, line])).values());
+
+    console.log(deduped);
+
+    return deduped;
+  });
 </script>
 
 <g class="line-group">
-  {#each renderedLines as line (line.id || line)}
+  {#each renderedLines as line (`${line.id}${line.d}`)}
     <g class="line" style:--line-dasharray={line.dasharray ? line.dasharray : undefined} transition:fade>
       <path class="path-line outline" d={line.d}></path>
       <path class="path-line" d={line.d} stroke={line.stroke}></path>
@@ -59,7 +69,7 @@
 
     &.outline {
       stroke-width: 3px;
-      stroke: #fff;
+      stroke: transparent;
     }
   }
 </style>
